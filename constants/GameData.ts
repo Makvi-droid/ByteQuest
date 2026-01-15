e719aa3e34594148d5232fc58975ad1a34ebc91d
Makvi-droid <pasbenauro05@gmail.com>
2026-01-15 13:18:06 +0800

    making an object about info of levels

---
+++ constants/GameData.ts
@@ -0,0 +1,9 @@
+export interface LevelData {
+    id: number;
+    monsterName: String;
+    monsterImg: any;
+    prompt: String;
+    correctAnswer: String;
+    suffix: String;
+    damage: number;
+}
